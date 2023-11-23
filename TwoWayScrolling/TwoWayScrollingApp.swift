10e9c67792de85bde50906030a0ec4950896207b
DVdroid <anandin02@gmail.com>
2023-11-23 21:10:39 +0530

    Initial Commit

---
+++ TwoWayScrolling/TwoWayScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayScrollingApp.swift
+//  TwoWayScrolling
+//
+//  Created by Vikash Anand on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
